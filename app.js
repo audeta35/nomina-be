0adc6410993ba7a47549cc07e83fbddc0d71f1d8
audeta35 <audeta35@gmail.com>
2023-03-15 19:18:14 +0700

    #dev: change port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const server = express();
 const app = require('http').Server(server);
 const cors = require('cors');
---
+++ readme.md
@@ -0,0 +1,9 @@
+#Dokumentasi
+
+- project menggunakan express js
+
+* Instalasi
+- clone repo
+- yarn / npm install
+- yarn / npm start
+- default port 3001
